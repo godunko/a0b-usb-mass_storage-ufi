c1a74261cb9c489aa57559a69915960e2f21be3d
Vadim Godunko <vgodunko@gmail.com>
2025-03-01 20:17:49 +0400

    TEST_UNIT_READY

--- source/a0b-usb-mass_storage-ufi-protocol.ads
+++ source/a0b-usb-mass_storage-ufi-protocol.ads
@@ -14,21 +14,21 @@ is
 
    --  type Operation_Code_Type is new A0B.Types.Unsigned_8;
 
-   --  Format Unit_Operation_Code                  : 04#
+   TEST_UNIT_READY_Operation_Code              : constant := 16#00#;
    INQUIRY_Operation_Code                      : constant := 16#12#;
+   --  READ_CAPACITY_Operation_Code                : constant := 16#25#;
+   --  Format Unit_Operation_Code                  : 04#
    --  Mode Select_Operation_Code                  : 55
    --  Mode Sense_Operation_Code                   : 5A
    --  Prevent-Allow Medium Removal_Operation_Code : 1E
    --  Read(10)_Operation_Code                     : 28
    --  Read(12)_Operation_Code                     : A8
-   --  Read Capacity_Operation_Code                : 25
    --  Read Format Capacities_Operation_Code       : 23
    --  Request Sense_Operation_Code                : 03
    --  Rezero_Operation_Code                       : 01
    --  Seek(10)_Operation_Code                     : 2B
    --  Send Diagnostic_Operation_Code              : 1D
    --  Start-Stop Unit_Operation_Code              : 1B
-   --  Test Unit Ready_Operation_Code              : 00
    --  Verify_Operation_Code                       : 2F
    --  Write(10)_Operation_Code                    : 2A
    --  Write(12)_Operation_Code                    : AA
@@ -60,35 +60,35 @@ is
    --  end record;
 
    type Command_Block is record
-      Operation_Code        : A0B.Types.Unsigned_8;
-      Logical_Unit_Number   : A0B.Types.Unsigned_3;
-      Reserved_1            : A0B.Types.Reserved_5;
-      Reserved_2            : A0B.Types.Reserved_8;
-      Reserved_3            : A0B.Types.Reserved_8;
-      Reserved_4            : A0B.Types.Reserved_8;
-      Reserved_5            : A0B.Types.Reserved_8;
-      Reserved_6            : A0B.Types.Reserved_8;
-      Reserved_7            : A0B.Types.Reserved_8;
-      Reserved_8            : A0B.Types.Reserved_8;
-      Reserved_9            : A0B.Types.Reserved_8;
-      Reserved_10           : A0B.Types.Reserved_8;
-      Reserved_11           : A0B.Types.Reserved_8;
+      Operation_Code      : A0B.Types.Unsigned_8;
+      Logical_Unit_Number : A0B.Types.Unsigned_3;
+      Reserved_1          : A0B.Types.Reserved_5;
+      Reserved_2          : A0B.Types.Reserved_8;
+      Reserved_3          : A0B.Types.Reserved_8;
+      Reserved_4          : A0B.Types.Reserved_8;
+      Reserved_5          : A0B.Types.Reserved_8;
+      Reserved_6          : A0B.Types.Reserved_8;
+      Reserved_7          : A0B.Types.Reserved_8;
+      Reserved_8          : A0B.Types.Reserved_8;
+      Reserved_9          : A0B.Types.Reserved_8;
+      Reserved_10         : A0B.Types.Reserved_8;
+      Reserved_11         : A0B.Types.Reserved_8;
    end record with Size => 8 * 12;
 
    for Command_Block use record
-      Operation_Code        at 0 range 0 .. 7;
-      Reserved_1            at 1 range 0 .. 4;
-      Logical_Unit_Number   at 1 range 5 .. 7;
-      Reserved_2            at 2 range 0 .. 7;
-      Reserved_3            at 3 range 0 .. 7;
-      Reserved_4            at 4 range 0 .. 7;
-      Reserved_5            at 5 range 0 .. 7;
-      Reserved_6            at 6 range 0 .. 7;
-      Reserved_7            at 7 range 0 .. 7;
-      Reserved_8            at 8 range 0 .. 7;
-      Reserved_9            at 9 range 0 .. 7;
-      Reserved_10           at 10 range 0 .. 7;
-      Reserved_11           at 11 range 0 .. 7;
+      Operation_Code      at 0 range 0 .. 7;
+      Reserved_1          at 1 range 0 .. 4;
+      Logical_Unit_Number at 1 range 5 .. 7;
+      Reserved_2          at 2 range 0 .. 7;
+      Reserved_3          at 3 range 0 .. 7;
+      Reserved_4          at 4 range 0 .. 7;
+      Reserved_5          at 5 range 0 .. 7;
+      Reserved_6          at 6 range 0 .. 7;
+      Reserved_7          at 7 range 0 .. 7;
+      Reserved_8          at 8 range 0 .. 7;
+      Reserved_9          at 9 range 0 .. 7;
+      Reserved_10         at 10 range 0 .. 7;
+      Reserved_11         at 11 range 0 .. 7;
    end record;
 
    type INQUIRY_Command_Block is record
@@ -167,6 +167,38 @@ is
       Vendor_Information     at 8 range 0 .. 8 * 8 - 1;
       Product_Identification at 16 range 0 .. 8 * 16 - 1;
       Product_Revision_Level at 32 range 0 .. 8 * 4 - 1;
+   end record;
+
+   type TEST_UNIT_READY_Command_Block is record
+      Operation_Code      : A0B.Types.Unsigned_8;
+      Logical_Unit_Number : A0B.Types.Unsigned_3;
+      Reserved_1          : A0B.Types.Reserved_5;
+      Reserved_2          : A0B.Types.Reserved_8;
+      Reserved_3          : A0B.Types.Reserved_8;
+      Reserved_4          : A0B.Types.Reserved_8;
+      Reserved_5          : A0B.Types.Reserved_8;
+      Reserved_6          : A0B.Types.Reserved_8;
+      Reserved_7          : A0B.Types.Reserved_8;
+      Reserved_8          : A0B.Types.Reserved_8;
+      Reserved_9          : A0B.Types.Reserved_8;
+      Reserved_10         : A0B.Types.Reserved_8;
+      Reserved_11         : A0B.Types.Reserved_8;
+   end record with Size => 8 * 12;
+
+   for TEST_UNIT_READY_Command_Block use record
+      Operation_Code        at 0 range 0 .. 7;
+      Reserved_1            at 1 range 0 .. 4;
+      Logical_Unit_Number   at 1 range 5 .. 7;
+      Reserved_2            at 2 range 0 .. 7;
+      Reserved_3            at 3 range 0 .. 7;
+      Reserved_4            at 4 range 0 .. 7;
+      Reserved_5            at 5 range 0 .. 7;
+      Reserved_6            at 6 range 0 .. 7;
+      Reserved_7            at 7 range 0 .. 7;
+      Reserved_8            at 8 range 0 .. 7;
+      Reserved_9            at 9 range 0 .. 7;
+      Reserved_10           at 10 range 0 .. 7;
+      Reserved_11           at 11 range 0 .. 7;
    end record;
 
 end A0B.USB.Mass_Storage.UFI.Protocol;
