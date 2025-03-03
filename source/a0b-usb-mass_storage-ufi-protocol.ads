de028a2fd875f2d9bbd6f6af3c6664fb385a378a
Vadim Godunko <vgodunko@gmail.com>
2025-03-03 18:00:53 +0400

    MODE_SENSE

--- source/a0b-usb-mass_storage-ufi-protocol.ads
+++ source/a0b-usb-mass_storage-ufi-protocol.ads
@@ -20,9 +20,9 @@ is
    TEST_UNIT_READY_Operation_Code              : constant := 16#00#;
    INQUIRY_Operation_Code                      : constant := 16#12#;
    READ_CAPACITY_Operation_Code                : constant := 16#25#;
+   MODE_SENSE_Operation_Code                   : constant := 16#5A#;
    --  Format Unit_Operation_Code                  : 04#
    --  Mode Select_Operation_Code                  : 55
-   --  Mode Sense_Operation_Code                   : 5A
    --  Prevent-Allow Medium Removal_Operation_Code : 1E
    --  Read(10)_Operation_Code                     : 28
    --  Read(12)_Operation_Code                     : A8
@@ -152,6 +152,285 @@ is
       Product_Identification at 16 range 0 .. 8 * 16 - 1;
       Product_Revision_Level at 32 range 0 .. 8 * 4 - 1;
    end record;
+
+   Media_Type_And_Write_Protect_Page      : constant := 16#00#;
+   Read_Write_Error_Recovery_Page         : constant := 16#01#;
+   Flexible_Disk_Page                     : constant := 16#05#;
+   Removable_Block_Access_Capacities_Page : constant := 16#1B#;
+   Timer_And_Protect_Page                 : constant := 16#1C#;
+   All_Pages                              : constant := 16#3F#;
+
+   type MODE_SENSE_Command_Block is record
+      Operation_Code           : A0B.Types.Unsigned_8;
+      Logical_Unit_Number      : A0B.Types.Unsigned_3;
+      Reserved_1_4_4           : A0B.Types.Reserved_1;
+      Disable_Block_Descriptor : Boolean := False;
+      Reserved_1_0_2           : A0B.Types.Reserved_1;
+      Page_Control             : A0B.Types.Unsigned_2;
+      Page_Code                : A0B.Types.Unsigned_6;
+      Reserved_3               : A0B.Types.Reserved_8;
+      Reserved_4               : A0B.Types.Reserved_8;
+      Reserved_5               : A0B.Types.Reserved_8;
+      Reserved_6               : A0B.Types.Reserved_8;
+      Parameter_List_Length    : A0B.Types.Big_Endian.Unsigned_16;
+      Reserved_9               : A0B.Types.Reserved_8;
+      Reserved_10              : A0B.Types.Reserved_8;
+      Reserved_11              : A0B.Types.Reserved_8;
+   end record
+     with Size      => 12 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for MODE_SENSE_Command_Block use record
+      Operation_Code           at 0 range 0 .. 7;
+      Reserved_1_0_2           at 1 range 0 .. 2;
+      Disable_Block_Descriptor at 1 range 3 .. 3;
+      Reserved_1_4_4           at 1 range 4 .. 4;
+      Logical_Unit_Number      at 1 range 5 .. 7;
+      Page_Code                at 2 range 0 .. 5;
+      Page_Control             at 2 range 6 .. 7;
+      Reserved_3               at 3 range 0 .. 7;
+      Reserved_4               at 4 range 0 .. 7;
+      Reserved_5               at 5 range 0 .. 7;
+      Reserved_6               at 6 range 0 .. 7;
+      Parameter_List_Length    at 7 range 0 .. 15;
+      Reserved_9               at 9 range 0 .. 7;
+      Reserved_10              at 10 range 0 .. 7;
+      Reserved_11              at 11 range 0 .. 7;
+   end record;
+
+   type Mode_Parameter_Header_Block is record
+      Mode_Data_Length : A0B.Types.Big_Endian.Unsigned_16 := (Value => 0);
+      Medium_Type_Code : A0B.Types.Unsigned_8;
+      Write_Protected  : Boolean;
+      Reserved_3_5_6   : A0B.Types.Reserved_2;
+      DPOFUA           : Boolean                          := False;
+      Reserved_3_0_3   : A0B.Types.Reserved_4;
+      Reserved_4       : A0B.Types.Reserved_8;
+      Reserved_5       : A0B.Types.Reserved_8;
+      Reserved_6       : A0B.Types.Reserved_8;
+      Reserved_7       : A0B.Types.Reserved_8;
+   end record
+     with Size      => 8 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for Mode_Parameter_Header_Block use record
+      Mode_Data_Length at 0 range 0 .. 15;
+      Medium_Type_Code at 2 range 0 .. 7;
+      Reserved_3_0_3   at 3 range 0 .. 3;
+      DPOFUA           at 3 range 4 .. 4;
+      Reserved_3_5_6   at 3 range 5 .. 6;
+      Write_Protected  at 3 range 7 .. 7;
+      Reserved_4       at 4 range 0 .. 7;
+      Reserved_5       at 5 range 0 .. 7;
+      Reserved_6       at 6 range 0 .. 7;
+      Reserved_7       at 7 range 0 .. 7;
+   end record;
+
+   type Read_Write_Error_Recovery_Page_Block is record
+      Parameters_Savable                  : A0B.Types.Unsigned_1 := 0;
+      Reserved_0                          : A0B.Types.Reserved_1;
+      Page_Code                           : A0B.Types.Unsigned_6 :=
+        Read_Write_Error_Recovery_Page;
+      Page_Length                         : A0B.Types.Unsigned_8 := 16#0A#;
+      Automatic_Write_Reallocation_Enable : A0B.Types.Unsigned_1 := 0;
+      Reserved_2_5_6                      : A0B.Types.Reserved_2;
+      Read_Continuous                     : A0B.Types.Unsigned_1 := 0;
+      Post_Error                          : Boolean;
+      Reserved_2_1_1                      : A0B.Types.Reserved_1;
+      Disable_Correction                  : A0B.Types.Unsigned_1 := 0;
+      Read_Retry_Count                    : A0B.Types.Unsigned_8;
+      Reserved_4                          : A0B.Types.Reserved_8;
+      Reserved_5                          : A0B.Types.Reserved_8;
+      Reserved_6                          : A0B.Types.Reserved_8;
+      Reserved_7                          : A0B.Types.Reserved_8;
+      Write_Retry_Count                   : A0B.Types.Unsigned_8;
+      Reserved_9                          : A0B.Types.Reserved_8;
+      Reserved_10                         : A0B.Types.Reserved_8;
+      Reserved_11                         : A0B.Types.Reserved_8;
+   end record
+     with Size      => 12 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for Read_Write_Error_Recovery_Page_Block use record
+      Page_Code                           at 0 range 0 .. 5;
+      Reserved_0                          at 0 range 6 .. 6;
+      Parameters_Savable                  at 0 range 7 .. 7;
+      Page_Length                         at 1 range 0 .. 7;
+      Disable_Correction                  at 2 range 0 .. 0;
+      Reserved_2_1_1                      at 2 range 1 .. 1;
+      Post_Error                          at 2 range 2 .. 2;
+      Read_Continuous                     at 2 range 3 .. 3;
+      Reserved_2_5_6                      at 2 range 4 .. 6;
+      Automatic_Write_Reallocation_Enable at 2 range 7 .. 7;
+      Read_Retry_Count                    at 3 range 0 .. 7;
+      Reserved_4                          at 4 range 0 .. 7;
+      Reserved_5                          at 5 range 0 .. 7;
+      Reserved_6                          at 6 range 0 .. 7;
+      Reserved_7                          at 7 range 0 .. 7;
+      Write_Retry_Count                   at 8 range 0 .. 7;
+      Reserved_9                          at 9 range 0 .. 7;
+      Reserved_10                         at 10 range 0 .. 7;
+      Reserved_11                         at 11 range 0 .. 7;
+   end record;
+
+   type Flexible_Disk_Page_Block is record
+      Parameters_Savable    : A0B.Types.Unsigned_1 := 0;
+      Reserved_0            : A0B.Types.Reserved_1;
+      Page_Code             : A0B.Types.Unsigned_6 := Flexible_Disk_Page;
+      Page_Length           : A0B.Types.Unsigned_8 := 16#1E#;
+      Transfer_Rate         : A0B.Types.Big_Endian.Unsigned_16;
+      Number_Of_Heads       : A0B.Types.Unsigned_8 := 2;
+      Sectors_Per_Track     : A0B.Types.Unsigned_8;
+      Data_Bytes_Per_Sector : A0B.Types.Unsigned_16;
+      Number_Of_Cylinders   : A0B.Types.Unsigned_16;
+      Reserved_10           : A0B.Types.Reserved_8;
+      Reserved_11           : A0B.Types.Reserved_8;
+      Reserved_12           : A0B.Types.Reserved_8;
+      Reserved_13           : A0B.Types.Reserved_8;
+      Reserved_14           : A0B.Types.Reserved_8;
+      Reserved_15           : A0B.Types.Reserved_8;
+      Reserved_16           : A0B.Types.Reserved_8;
+      Reserved_17           : A0B.Types.Reserved_8;
+      Reserved_18           : A0B.Types.Reserved_8;
+      Motor_On_Delay        : A0B.Types.Unsigned_8 := 5;
+      Motor_Off_Delay       : A0B.Types.Unsigned_8 := 30;
+      Reserved_21           : A0B.Types.Reserved_8;
+      Reserved_22           : A0B.Types.Reserved_8;
+      Reserved_23           : A0B.Types.Reserved_8;
+      Reserved_24           : A0B.Types.Reserved_8;
+      Reserved_25           : A0B.Types.Reserved_8;
+      Reserved_26           : A0B.Types.Reserved_8;
+      Reserved_27           : A0B.Types.Reserved_8;
+      Medium_Rotation_Rate  : A0B.Types.Unsigned_16;
+      Reserved_30           : A0B.Types.Reserved_8;
+      Reserved_31           : A0B.Types.Reserved_8;
+   end record
+     with Size      => 32 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for Flexible_Disk_Page_Block use record
+      Page_Code             at 0 range 0 .. 5;
+      Reserved_0            at 0 range 6 .. 6;
+      Parameters_Savable    at 0 range 7 .. 7;
+      Page_Length           at 1 range 0 .. 7;
+      Transfer_Rate         at 2 range 0 .. 15;
+      Number_Of_Heads       at 4 range 0 .. 7;
+      Sectors_Per_Track     at 5 range 0 .. 7;
+      Data_Bytes_Per_Sector at 6 range 0 .. 15;
+      Number_Of_Cylinders   at 8 range 0 .. 15;
+      Reserved_10           at 10 range 0 .. 7;
+      Reserved_11           at 11 range 0 .. 7;
+      Reserved_12           at 12 range 0 .. 7;
+      Reserved_13           at 13 range 0 .. 7;
+      Reserved_14           at 14 range 0 .. 7;
+      Reserved_15           at 15 range 0 .. 7;
+      Reserved_16           at 16 range 0 .. 7;
+      Reserved_17           at 17 range 0 .. 7;
+      Reserved_18           at 18 range 0 .. 7;
+      Motor_On_Delay        at 19 range 0 .. 7;
+      Motor_Off_Delay       at 20 range 0 .. 7;
+      Reserved_21           at 21 range 0 .. 7;
+      Reserved_22           at 22 range 0 .. 7;
+      Reserved_23           at 23 range 0 .. 7;
+      Reserved_24           at 24 range 0 .. 7;
+      Reserved_25           at 25 range 0 .. 7;
+      Reserved_26           at 26 range 0 .. 7;
+      Reserved_27           at 27 range 0 .. 7;
+      Medium_Rotation_Rate  at 28 range 0 .. 15;
+      Reserved_30           at 30 range 0 .. 7;
+      Reserved_31           at 31 range 0 .. 7;
+   end record;
+
+   type Removable_Block_Access_Capabilities_Page_Block is record
+      Parameters_Savable                 : A0B.Types.Unsigned_1 := 0;
+      Reserved_0                         : A0B.Types.Reserved_1;
+      Page_Code                          : A0B.Types.Unsigned_6 :=
+        Removable_Block_Access_Capacities_Page;
+      Page_Length                        : A0B.Types.Unsigned_8 := 16#0A#;
+      System_Floppy_Type                 : Boolean;
+      Supports_Reporting_Format_Progress : A0B.Types.Unsigned_1 := 0;
+      Reserved_2                         : A0B.Types.Reserved_6;
+      Non_CD                             : A0B.Types.Unsigned_1 := 0;
+      Single_Multiple_LUN                : A0B.Types.Unsigned_1 := 0;
+      Reserved_3                         : A0B.Types.Reserved_3;
+      Total_Number_Of_Logical_Units      : A0B.Types.Unsigned_3 := 1;
+      Reserved_4                         : A0B.Types.Reserved_8;
+      Reserved_5                         : A0B.Types.Reserved_8;
+      Reserved_6                         : A0B.Types.Reserved_8;
+      Reserved_7                         : A0B.Types.Reserved_8;
+      Reserved_8                         : A0B.Types.Reserved_8;
+      Reserved_9                         : A0B.Types.Reserved_8;
+      Reserved_10                        : A0B.Types.Reserved_8;
+      Reserved_11                        : A0B.Types.Reserved_8;
+   end record
+     with Size      => 12 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for  Removable_Block_Access_Capabilities_Page_Block use record
+      Page_Code                          at 0 range 0 .. 5;
+      Reserved_0                         at 0 range 6 .. 6;
+      Parameters_Savable                 at 0 range 7 .. 7;
+      Page_Length                        at 1 range 0 .. 7;
+      Reserved_2                         at 2 range 0 .. 5;
+      Supports_Reporting_Format_Progress at 2 range 6 .. 6;
+      System_Floppy_Type                 at 2 range 7 .. 7;
+      Total_Number_Of_Logical_Units      at 3 range 0 .. 2;
+      Reserved_3                         at 3 range 3 .. 5;
+      Single_Multiple_LUN                at 3 range 6 .. 6;
+      Non_CD                             at 3 range 7 .. 7;
+      Reserved_4                         at 4 range 0 .. 7;
+      Reserved_5                         at 5 range 0 .. 7;
+      Reserved_6                         at 6 range 0 .. 7;
+      Reserved_7                         at 7 range 0 .. 7;
+      Reserved_8                         at 8 range 0 .. 7;
+      Reserved_9                         at 9 range 0 .. 7;
+      Reserved_10                        at 10 range 0 .. 7;
+      Reserved_11                        at 11 range 0 .. 7;
+   end record;
+
+   type Timer_And_Protect_Page_Block is record
+      Parameters_Savable                 : A0B.Types.Unsigned_1 := 0;
+      Reserved_0                         : A0B.Types.Reserved_1;
+      Page_Code                          : A0B.Types.Unsigned_6 :=
+        Timer_And_Protect_Page;
+      Page_Length                        : A0B.Types.Unsigned_8 := 16#06#;
+      Reserved_2                         : A0B.Types.Reserved_8;
+      Reserved_3                         : A0B.Types.Reserved_4;
+      Inactivity_Time_Multiplier         : A0B.Types.Unsigned_4;
+      Reserved_4                         : A0B.Types.Reserved_6;
+      Disable_Media_Access_Until_Power   : A0B.Types.Unsigned_1 := 0;
+      Software_Write_Protect_Until_Power : A0B.Types.Unsigned_1 := 0;
+      Reserved_5                         : A0B.Types.Reserved_8;
+      Reserved_6                         : A0B.Types.Reserved_8;
+      Reserved_7                         : A0B.Types.Reserved_8;
+   end record
+     with Size      => 8 * A0B.Types.Unsigned_8'Size,
+          Bit_Order => System.Low_Order_First;
+
+   for Timer_And_Protect_Page_Block use record
+      Page_Code                          at 0 range 0 .. 5;
+      Reserved_0                         at 0 range 6 .. 6;
+      Parameters_Savable                 at 0 range 7 .. 7;
+      Page_Length                        at 1 range 0 .. 7;
+      Reserved_2                         at 2 range 0 .. 7;
+      Inactivity_Time_Multiplier         at 3 range 0 .. 3;
+      Reserved_3                         at 3 range 4 .. 7;
+      Software_Write_Protect_Until_Power at 4 range 0 .. 0;
+      Disable_Media_Access_Until_Power   at 4 range 1 .. 1;
+      Reserved_4                         at 4 range 2 .. 7;
+      Reserved_5                         at 5 range 0 .. 7;
+      Reserved_6                         at 6 range 0 .. 7;
+      Reserved_7                         at 7 range 0 .. 7;
+   end record;
+
+   type Mode_Parameter_List is record
+      Header                            : Mode_Parameter_Header_Block;
+      Read_Write_Error_Recovery         : Read_Write_Error_Recovery_Page_Block;
+      Flexible_Disk                     : Flexible_Disk_Page_Block;
+      Removable_Block_Access_Capacities :
+        Removable_Block_Access_Capabilities_Page_Block;
+      Timer_And_Protect                 : Timer_And_Protect_Page_Block;
+   end record with Size => 72 * A0B.Types.Unsigned_8'Size;
 
    type READ_CAPACITY_Command_Block is record
       Operation_Code        : A0B.Types.Unsigned_8;
